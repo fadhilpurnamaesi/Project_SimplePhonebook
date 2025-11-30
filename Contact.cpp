e49e4d88f972020ffe7b91fc894066cf6f94f4ac
FadhilPurnama <fadhilpurnamaesi2006@mail.ugm.ac.id>
2025-12-01 07:06:55 +0800

    Update Contact.cpp

--- Contact.cpp
+++ Contact.cpp
@@ -12,12 +12,12 @@ Contact::Contact()
     email = "";
 }
 
-Contact::Contact(string first, string last, string number, string mail)
+Contact::Contact(string first, string last, string number, string email)
 {
     firstName = first;
     lastName = last;
     phoneNumber = number;
-    email = mail;
+    email = email;
 }
 
 string Contact::getFirstName() const { return firstName; }
@@ -28,4 +28,4 @@ string Contact::getEmail() const { return email; }
 void Contact::setFirstName(string first) { firstName = first; }
 void Contact::setLastName(string last) { lastName = last; }
 void Contact::setPhoneNumber(string number) { phoneNumber = number; }
-void Contact::setEmail(string mail) { email = mail; }
+void Contact::setEmail(string mail) { email = email; }
